bd21382774f3478e65676cd21bc66d61707b1ee6
Dinesh Wijekoon <dinesh707@gmail.com>
2017-03-03 11:03:45 +0200

    initial strucutre, and schema cleanup

---
+++ src/com/dinesh/squery/Column.java
@@ -0,0 +1,12 @@
+package com.dinesh.squery;
+
+import java.util.List;
+
+public class Column {
+  
+  //TODO : may be an enum ??
+  private String dataType;
+  
+  private List<?> data;
+
+}
---
+++ src/com/dinesh/squery/Table.java
@@ -0,0 +1,10 @@
+package com.dinesh.squery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Table {
+  
+  List<Column> columns = new ArrayList<Column>();
+
+}
